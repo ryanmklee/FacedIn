ded07064340d1cd446396690f2fd04f553869443
Ryan Lee <ryanmkitlee@gmail.com>
2019-02-28 12:03:51 -0800

    initial db code

--- project.py
+++ project.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import jsonify, request, make_response, send_from_directory
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return send_from_directory('templates', 'login.html')
 
 
 if __name__ == '__main__':
