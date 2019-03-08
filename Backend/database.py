5b8fcd0a4ebc77b739b1bbfab2811c12116466b1
Ryan Lee <ryanmkitlee@gmail.com>
2019-03-07 22:22:49 -0800

    create_user api call built and database call

--- Backend/database.py
+++ Backend/database.py
@@ -6,17 +6,27 @@ PASSWORD = '8XaKBKymJa295Ej28nnXiYFnUlDWkApM'
 HOST = 'stampy.db.elephantsql.com'
 
 
-INSERT_USER_SQL = ''
+INSERT_USER_SQL = '''INSERT INTO users (user_id, email, password) VALUES (DEFAULT, '{usr}', '{pwd}')'''
 
 
 def get_connection():
-    try:
-        conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
-    except psycopg2.Error as e:
-        # TODO: need logger
-        print('failed to connect')
+    """
+    Connects to PostgreSQL database
+    :return: psycopg2 connection object
+    """
+    conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
+    return conn if conn else psycopg2.Error
 
-def insert_user(user, password):
-    # TODO: implement
-    pass
+
+def insert_user(connection, user: str, password: str) -> None:
+    """
+    Insert user into database with password
+    :param connection: Database connection
+    :param user: str
+    :param password: str
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(INSERT_USER_SQL.format(usr=user, pwd=password))
+        connection.commit()
+    
 
--- Backend/project.py
+++ Backend/project.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import jsonify, request, make_response, send_from_directory
+from flask_api import status
 
+from backend import database
 
 app = Flask(__name__)
 
@@ -8,6 +10,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return send_from_directory('templates', 'login.html')
+
+
+@app.route('/api/create', methods=['POST'])
+def create_user():
+    """
+    POST request to create user
+    PARAMS: user, password
+    :return: status code
+    """
+    args = request.args
+    user = args['user']
+    pwd = args['password']
+    if user and pwd:
+        with database.get_connection() as conn:
+            database.insert_user(conn, user, pwd)
+    else:
+        return jsonify(status=status.HTTP_400_BAD_REQUEST)
+
+    return jsonify(status=status.HTTP_201_CREATED)
 
 
 if __name__ == '__main__':
