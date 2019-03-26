50475288444f7d3f4d81cdcff5b4b9bf68dc30d7
Seiyoung Ahn <sedddong@gmail.com>
2019-03-26 15:37:23 -0700

    Update Event component

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -3,26 +3,24 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const monthNames = [
-  "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+import formatDateString from '../utils/date';
 
 export default class Event extends Component {
-  getDateString() {
-    return monthNames[this.props.event.date.getMonth()] + " " + this.props.event.date.getDate();
+  constructor(props) {
+    super(props);
+    this.date = formatDateString(this.props.event.time);
   }
 
   render() {
     return (
       <Container>
         <Row>
-          <Col sm={2}>
-            {this.getDateString()}
+          <Col sm={2} className="date">
+            {this.date}
           </Col>
           <Col>
             <Row>
-              <h6>{this.props.event.name}</h6>
+              <h6>{this.props.event.event_name}</h6>
             </Row>
             <Row>
               Location: {this.props.event.location}
