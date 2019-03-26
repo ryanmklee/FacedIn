ef89d7c76261d3f4bbfd9081fff938836cbab58d
ghammer <gavinstanleyham@gmail.com>
2019-03-26 01:06:54 -0700

    create user page almost there

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,26 +1,46 @@
 import React from "react";
 import {connect} from "react-redux"
-import constant, {PASSWORD_INPUT, USERNAME_INPUT} from "../constants/actionTypes";
+import {PASSWORD_CREATE_INPUT, USERNAME_CREATE_INPUT} from "../constants/actionTypes";
 import store from "../store/index"
-import axios from 'axios'
-import {tryLogin} from "../actions/login";
+import {createUser} from "../actions/createuser";
+import {Redirect} from "react-router-dom";
+
 class CreateUser extends React.Component {
 
     constructor(props) {
         super(props);
+        this.submitUserClick = this.submitUserClick.bind(this);
     }
+
+    submitUserClick = (username, password) => {
+        store.dispatch(createUser(username, password)).then(() => {
+            this.render()
+        });
+    };
 
 
     render() {
+        if (this.props.createdUser) {
+            return <Redirect push to="/home"/>
+        }
         return (
-                <h3>CreateUser</h3>
+            <div>
+                <h3>Create User</h3>
+                <input id = {USERNAME_CREATE_INPUT} type="text" ref="username" placeholder="Email" />
+                <input id = {PASSWORD_CREATE_INPUT} type="password" ref="password" placeholder="Password" />
+                <button type="button" onClick={()=> {
+                    const username = document.getElementById(USERNAME_CREATE_INPUT).value;
+                    const password = document.getElementById(PASSWORD_CREATE_INPUT).value;
+                    this.submitUserClick(username, password)
+                }}>Submit</button>
+            </div>
         );
     }
 }
 
 function mapStateToProps(state){
     return {
-
+        createdUser: state.createdUser
     }
 }
 export default connect(mapStateToProps)(CreateUser)
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import login from './login';
+import createuser from './createuser'
 export default combineReducers({
-    login
+    login,
+    createuser
 });
