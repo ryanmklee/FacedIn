67a5e6e1bf1c9f4a99635be88afa275a97bde3c0
Seiyoung Ahn <sedddong@gmail.com>
2019-03-27 23:34:13 -0700

    Add GroupPage and related component

---
+++ frontend/src/mockData/mockGroups.js
@@ -0,0 +1,18 @@
+const groups = [
+  {
+      "activity": "\"Rock climbing\"",
+      "admin": "Dr. Strange",
+      "group_id": 3,
+      "group_name": "\"Rock climbing heroes\"",
+      "user_id": 1
+  },
+  {
+      "activity": "Writing English papers",
+      "admin": "Lucy MacDonald",
+      "group_id": 4,
+      "group_name": "Writing Club",
+      "user_id": 8
+  }
+];
+
+export default groups
